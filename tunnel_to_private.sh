a62b382cffbc3034c8470531565821ac4ab3a62a
Quan Chau <quan.chau@appfolio.com>
2020-03-20 09:26:07 -0700

    update instruction in tunnel_to_private.sh

--- tunnel_to_private.sh
+++ tunnel_to_private.sh
@@ -1,6 +1,6 @@
 # USAGE: bash tunnel_to_private.sh <Public DNS/IP address> <Private DNS/IP address> <port on private instance> <local port>
 # Then you can open the page on 127.0.0.1:<local port>
 
-# For Kibana, uses 127.0.0.1:<local port>/_plugin/kibana
+# For AWS Elasticsearch, port on private instance is 80, uses 127.0.0.1:<local port>/_plugin/kibana for Kibana
 
 ssh -L 127.0.0.1:$4:$2:$3 centos@$1
