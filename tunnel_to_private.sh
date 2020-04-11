b6e4177d3eec00703fcdd804c452dc4629de6081
Quan Chau <quan.chau@appfolio.com>
2020-04-11 11:32:03 -0700

    add argument about user on public instance to tunnel_to_private.sh

--- tunnel_to_private.sh
+++ tunnel_to_private.sh
@@ -1,6 +1,4 @@
-# USAGE: bash tunnel_to_private.sh <Public DNS/IP address> <Private DNS/IP address> <port on private instance> <local port>
+# USAGE: bash tunnel_to_private.sh <Public DNS/IP address> <Private DNS/IP address> <port on private instance> <local port> <user on public instance>
 # Then you can open the page on 127.0.0.1:<local port>
 
-# For AWS Elasticsearch, port on private instance is 80, uses 127.0.0.1:<local port>/_plugin/kibana for Kibana
-
-ssh -L 127.0.0.1:$4:$2:$3 centos@$1
+ssh -L 127.0.0.1:$4:$2:$3 $5@$1 -fN
