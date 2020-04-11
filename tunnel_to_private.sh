c1d6e9eb5d5a7bd9daaac78997c3b93563dfd8f0
Quan Chau <quan.chau@appfolio.com>
2020-04-11 11:47:16 -0700

    combine user and host of public jumpbox into same argument

--- tunnel_to_private.sh
+++ tunnel_to_private.sh
@@ -1,4 +1,4 @@
-# USAGE: bash tunnel_to_private.sh <Public DNS/IP address> <Private DNS/IP address> <port on private instance> <local port> <user on public instance>
+# USAGE: bash tunnel_to_private.sh <user@Public DNS/IP address> <Private DNS/IP address> <port on private instance> <local port>
 # Then you can open the page on 127.0.0.1:<local port>
 
-ssh -L 127.0.0.1:$4:$2:$3 $5@$1 -fN
+ssh -L 127.0.0.1:$4:$2:$3 $1 -fN
