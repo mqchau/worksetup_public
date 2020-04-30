5aa8a8c08fab05be7d59fac7def7d9af4e11b7bf
Quan Chau <quan.chau@appfolio.com>
2020-04-30 07:44:57 -0700

    script to SSH with AWS credentials

---
+++ ssh_with_credentials.sh
@@ -0,0 +1,12 @@
+# NOTE: on the remote machine, you must edit sshd_config to make this work
+#   sudo vi /etc/ssh/sshd_config
+#   Add "AcceptEnv AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN" near other AcceptEnv
+
+# USAGE: bash ssh_with_credentials.sh <user@host>
+
+RAW_TEMP_CREDENTIALS=$(aws sts get-session-token)
+export AWS_ACCESS_KEY_ID=$(echo $RAW_TEMP_CREDENTIALS | jq -r .Credentials.AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$(echo $RAW_TEMP_CREDENTIALS | jq -r .Credentials.SecretAccessKey)
+export AWS_SESSION_TOKEN=$(echo $RAW_TEMP_CREDENTIALS | jq -r .Credentials.SessionToken)
+
+ssh  -o "SendEnv AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN" $1
