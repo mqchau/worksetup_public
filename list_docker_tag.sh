f803066fc2ca04b2215fa9b3aa63a923aadc4e39
Quan Chau <quan.chau@appfolio.com>
2020-05-01 15:45:53 -0700

    use env var for list docker tag

--- list_docker_tag.sh
+++ list_docker_tag.sh
@@ -1,3 +1,5 @@
-source ~/.credentials/dockerhub.sh
-wget -q https://$DOCKERHUB_USER:$DOCKERHUB_PASSWORD@registry.hub.docker.com/v1/repositories/$1/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}'
+# USAGE: bash list_docker_tags.sh <user_name:password> <repo> 
+#   User name and password of hub.docker.com
+
+wget -q https://$1@registry.hub.docker.com/v1/repositories/$2/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}'
 
