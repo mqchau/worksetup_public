cfbd55f8580c329682e5c41cd6383ea9a626da32
Quan Chau <quan.chau@appfolio.com>
2020-05-10 13:04:46 -0700

    update jupmbox_ssh script to enforce username

--- jumpbox_ssh.sh
+++ jumpbox_ssh.sh
@@ -1,3 +1,3 @@
-# USAGE: bash jumpbox_ssh.sh <public DNS/IP of jumpbox> <private DNS/IP of target>
+# USAGE: bash jumpbox_ssh.sh <user@public DNS/IP of jumpbox> <user@private DNS/IP of target>
 
-ssh -o "ProxyCommand ssh -W %h:%p centos@$1" centos@$2
+ssh -o "ProxyCommand ssh -W %h:%p $1" $2
