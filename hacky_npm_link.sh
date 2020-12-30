4cb33b2c0395d4b52bb10429a7746948bf066a7f
Quan Chau <quan.chau@appfolio.com>
2020-12-30 11:39:32 -0800

    fix bug on creating es folder under dist folder

--- hacky_npm_link.sh
+++ hacky_npm_link.sh
@@ -14,8 +14,8 @@ cd node_modules/$TARGET_DIRECTORY && \
   # hard link all files at surface level of $SOURCE_DIRECTORY
   find $SOURCE_DIRECTORY -maxdepth 1 -mindepth 1 -type f -not -name ".*" -exec ln {} \; && \
   # hard link all files at dist folder of $SOURCE_DIRECTORY
-  mkdir -p dist && cd dist && \
-  find $SOURCE_DIRECTORY/dist -maxdepth 1 -mindepth 1 -type f -not -name ".*" -exec ln {} \; && \
+  mkdir -p dist && pushd dist && \
+  find $SOURCE_DIRECTORY/dist -maxdepth 1 -mindepth 1 -type f -not -name ".*" -exec ln {} \; && popd && \
   # hard link all files at es folder of $SOURCE_DIRECTORY
-  mkdir -p es && cd es && \
-  find $SOURCE_DIRECTORY/es -maxdepth 1 -mindepth 1 -type f -not -name ".*" -exec ln {} \;
+  mkdir -p es && pushd es && \
+  find $SOURCE_DIRECTORY/es -maxdepth 1 -mindepth 1 -type f -not -name ".*" -exec ln {} \; && popd
