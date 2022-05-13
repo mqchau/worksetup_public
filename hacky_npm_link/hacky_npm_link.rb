2da63e60ccd635f1bcd3e7d2ac67191cef341aa3
Quan Chau <quan.chau@appfolio.com>
2022-05-13 11:21:02 -0700

    hacky npm link

---
+++ hacky_npm_link/hacky_npm_link.rb
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+  echo 1>&2 "ERROR: Not enough arguments"
+  echo "USAGE: bash hacky_npm_link.sh <name of package> <path to local repo of package>"
+  exit 2
+fi
+
+SOURCE_DIRECTORY=$2
+TARGET_DIRECTORY=$1
+
+cd node_modules/$TARGET_DIRECTORY && \
+  rm -rf * && \
+  # hard link all files at surface level of $SOURCE_DIRECTORY
+  find $SOURCE_DIRECTORY -maxdepth 1 -mindepth 1 -type f -not -name ".*" -exec ln {} \; && \
+  # hard link all files at dist folder of $SOURCE_DIRECTORY
+  mkdir -p dist && pushd dist && \
+  find $SOURCE_DIRECTORY/dist -maxdepth 1 -mindepth 1 -type f -not -name ".*" -exec ln {} \; && popd && \
+  # hard link all files & symbolic link all folders at es folder of $SOURCE_DIRECTORY
+  mkdir -p es && pushd es && \
+  find $SOURCE_DIRECTORY/es -maxdepth 1 -mindepth 1 -type f -not -name ".*" -exec ln -s {} \; && \
+  find $SOURCE_DIRECTORY/es -maxdepth 1 -mindepth 1 -type d -not -name ".*" -exec ln -s {} \; && popd
